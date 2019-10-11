b9daad99d800ae2160b6f0c7534449248f10c7be
Kazu <nakaaaachin0286@gmail.com>
2019-10-11 15:45:48 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_one :profile
+  accepts_nested_attributes_for :profile
+  before_create :build_default_profile
+
+  private
+  def build_default_profile
+    build_profile 
+    true
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  devise_for :users, :controllers => {
+    :sessions      => "users/sessions",
+    :registrations => "users/registrations",
+    :passwords     => "users/passwords",
+  }
+
+  devise_scope :user do
+    get 'profile_edit', to: 'users/registrations#profile_edit', as: 'profile_edit'
+    patch 'profile_update', to: 'users/registrations#profile_update', as: 'profile_update'
+  end
+  
+  resources :users, :only => [:show], as: :mypage
+end
